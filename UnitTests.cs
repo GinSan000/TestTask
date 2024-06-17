053886f182beb7dc3811ad3e3f9c7ca006a2bbbd
GinSan000 <kcptc848@gmail.com>
2024-06-17 21:32:58 +0500

    Update and rename UnitTest1.cs to UnitTests.cs

--- UnitTests.cs
+++ UnitTests.cs
@@ -8,7 +8,7 @@ namespace SquareTests
         [TestMethod]
         public void Circle_6_return_133_10()
         {
-            Circle circle = new Circle("Круг", 6);
+            Circle circle = new Circle("ДД‘ГіДѓ", 6);
             double expectedResult = 113.10;
             double actualResult = circle.GetSquare();
             Assert.AreEqual(expectedResult, actualResult);
@@ -16,7 +16,7 @@ namespace SquareTests
         [TestMethod]
         public void Circle_22_5_return_1590_43()
         {
-            Circle circle = new Circle("Круг", 22.5);
+            Circle circle = new Circle("ДД‘ГіДѓ", 22.5);
             double expectedResult = 1590.43;
             double actualResult = circle.GetSquare();
             Assert.AreEqual(expectedResult, actualResult);
@@ -28,7 +28,7 @@ namespace SquareTests
         [TestMethod]
         public void Triangle_5_4_3()
         {
-            Triangle triangle = new Triangle("Треугольник", 5, 4, 3);
+            Triangle triangle = new Triangle("Е‡Д‘ДєГіДѓГ®Г«ГјГ­ДЌД™", 5, 4, 3);
             double expectedResult = 6;
             double actualResult = triangle.GetSquare();
             Assert.AreEqual(expectedResult, actualResult);
@@ -36,7 +36,7 @@ namespace SquareTests
         [TestMethod]
         public void Triangle_11_10_3()
         {
-            Triangle triangle = new Triangle("Треугольник", 11, 10, 3);
+            Triangle triangle = new Triangle("Е‡Д‘ДєГіДѓГ®Г«ГјГ­ДЌД™", 11, 10, 3);
             double expectedResult = 14.70;
             double actualResult = triangle.GetSquare();
             Assert.AreEqual(expectedResult, actualResult);
@@ -44,7 +44,7 @@ namespace SquareTests
         [TestMethod]
         public void Triangle_10_10_10()
         {
-            Triangle triangle = new Triangle("Треугольник", 10, 10, 10);
+            Triangle triangle = new Triangle("Е‡Д‘ДєГіДѓГ®Г«ГјГ­ДЌД™", 10, 10, 10);
             double expectedResult = 43.30;
             double actualResult = triangle.GetSquare();
             Assert.AreEqual(expectedResult, actualResult);
@@ -52,7 +52,7 @@ namespace SquareTests
         [TestMethod]
         public void Triangle_2_6_9_IsTriangle()
         {
-            Triangle triangle = new Triangle("Треугольник", 5, 6, 9);
+            Triangle triangle = new Triangle("Е‡Д‘ДєГіДѓГ®Г«ГјГ­ДЌД™", 5, 6, 9);
             Assert.IsFalse(triangle.IsRightTriangle());
         }
     }
